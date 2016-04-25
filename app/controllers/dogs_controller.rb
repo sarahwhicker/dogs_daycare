cec981dcf110ba1d0841f8d038a518278d19116f
Sarah Whicker <bigchief@thunderheadstrategies.com>
2016-04-25 10:41:35 -0500

    Added owner

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -23,10 +23,29 @@ class DogsController < ApplicationController
   def edit
     @dog = Dog.find(params[:id])
   end
-  
+
+  def update
+    @dog = Dog.find(params[:id])
+    if @dog.update(dog_params)
+      redirect_to dogs_url, notice: "Successfully updated!"
+    else
+      flash.now[:notice] = "Something went wrwong. Didn't update your dog."
+      render:edit
+    end
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    if @dog.destroy
+      redirect_to dogs_url, notice: "Successfully deleted!"
+    else
+      redirect_to dogs_url, notice: "Could not delete."
+  end
+end
+
 private
   def dog_params
-  	params.require(:dog).permit(:name, :breed, :age)
+  	params.require(:dog).permit(:name, :breed, :age, :owner)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421145230) do
+ActiveRecord::Schema.define(version: 20160425153054) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160421145230) do
     t.string   "integer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "owner"
   end
 
 end
